e6fb44b8777335a925bfc24265ed4e7fd69b5e7e
Hsb511 <hugo-selle@hotmail.fr>
2018-11-23 14:08:41 +0100

    ajoute de la queue avec I

--- Make_request.py
+++ Make_request.py
@@ -7,11 +7,11 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare('AB')
+channel.queue_declare('BI')
 
 channel.basic_publish(exchange='',
 					  routing_key='hello',
-					  body='A,request')
+					  body='B,request')
 connection.close()
 
 #while(True):

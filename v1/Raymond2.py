ffb4c373699aa9d5b36037b84229069f20bcd81d
Paul DUBOT <pdubot@pc-df-804.priv.enst-bretagne.fr>
2018-11-24 17:15:37 +0100

    v1

---
+++ v1/Raymond2.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+import pika
+import sys
+import time
+
+using = False
+request_Q = []
+asked = False
+crashed = False
+init_time = time.time()
+
+#On initialise le noeud, le premier argument est son id, le deuxieme est son holder
+myid = sys.argv[1]
+print("node's id is: " + myid)
+global holder
+holder = sys.argv[2]
+print("node's holder is: " + holder)
+
+#On initie la connection et le channel
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+#On cree une queue par voisin, on inscrit manuellement le nom des queues
+neighbor1 = sys.argv[3]
+
+channel.queue_declare(queue=neighbor1+myid)
+print("node " + myid + " is listening to " + neighbor1)
+channel.queue_declare(queue=myid+neighbor1)
+print("node " + myid + " is writing on " + myid + neighbor1)
+
+#On cree les queues avec le voisin "I"
+channel.queue_declare(queue="I"+myid)
+print("node " + myid + " is listening to " + "I")
+channel.queue_declare(queue=myid+"I")
+print("node " + myid + " is writing on " + myid + "I")
+
+
+if (len(sys.argv) > 4):
+    channel.queue_declare(queue=sys.argv[4] + myid)
+    print("node " + myid + " is listening to " + sys.argv[4])
+    channel.queue_declare(queue=myid + sys.argv[4])
+    print("node " + myid + " is writing on " + myid + sys.argv[4])
+
+if (len(sys.argv) > 5):
+    channel.queue_declare(queue=sys.argv[5] + myid)
+    print("node " + myid + " is listening to " + sys.argv[5])
+    channel.queue_declare(queue=myid + sys.argv[5])
+    print("node " + myid + " is writing on " + myid + sys.argv[5])
+
+
+#if not message_type:
+#    print("error")
+#    sys.exit(1)
+
+#On definit la methode pour donner les privileges
+def give_privilege(current_holder):
+    global holder
+   
+    holder = current_holder
+    print ("trying give privilege to ", request_Q, current_holder, holder, using)
+    # je suis holder et capable de donner le privilege (pas en section critique)
+    if holder == myid and not using and request_Q != []:
+    #channel.queue_declare(queue= min(request_Q[0], myid) + max(request_Q[0], myid))
+    	channel.basic_publish(exchange='',
+		                      routing_key=myid+request_Q[0],
+		                      body = request_Q[0] + ',' + "privilege")
+	holder = request_Q[0]
+        request_Q.pop(0)
+	asked = False
+	print("done")
+    else: print("condition not accepted")
+	   
+
+#On definit la methode pour la reception des privileges
+def get_request(sender_id):
+    global holder
+    global asked
+    global using
+    print ("just received a request, holder is: "+holder+ " sender_id is: "+ sender_id +" , request_Q : ", request_Q, " , asked : ", asked)
+    if sender_id == holder:
+	channel.queue_declare(myid+"I")
+ 	channel.basic_publish(exchange='',
+                              routing_key=myid+"I",
+                              body = "[x] " + myid + " was already the holder") 
+    # transmission de request d'un noeud precedent au suivant
+    elif holder != myid and request_Q != [] and not asked:
+        channel.basic_publish(exchange='',
+                              routing_key=myid+holder,
+                              body = myid + ',' + "request")
+        request_Q.append(sender_id)
+        print("request transmission done")
+    # je suis holder et recois une request alors que je suis deja occupe avec la section critique
+    elif holder == myid and request_Q != [] and request_Q[0] != myid:
+        request_Q.append(sender_id)
+        time.sleep(2)
+        give_privilege(holder)
+        request_Q.pop(0)
+        print("i'm already doing sthg")
+    # je suis holder et recoit une request et ne fais rien
+    elif holder == myid and request_Q == []:
+	request_Q.append(sender_id)
+        give_privilege(holder)
+	
+    elif holder != myid and request_Q == []:
+        request_Q.append(sender_id)
+        print("initiating a request on queue: "+min(holder, myid) + max(holder, myid))
+        channel.basic_publish(exchange='',
+                              routing_key=myid+holder,
+                              body = myid + ',' + "request")
+        print("just made request")
+	
+
+def get_privilege():
+    global holder
+    print("just received privilege")
+    if request_Q[0] == myid:
+        request_Q.pop(0)
+        #todo entree dans section critique
+        using = True
+        asked = False
+        holder = myid
+        print("i'm using it")
+        channel.basic_publish(exchange='', routing_key=myid+"I", body="[x] "+myid+" is the current holder of the privilege")
+
+    else:
+        holder = myid
+        give_privilege(holder)
+        print("just gave privilege")
+        using = False
+
+#On definit la methode de callback principale
+def callback(ch, method, properties, body):
+    global crashed
+    #On regarde il y a combien de temps le noeud a ete lance
+    #Si le message etait en attente et qu'on a pas deja dit qu'on avait crash
+    if time.time() - init_time < 1 and not crashed :
+	channel.basic_publish(exchange='', routing_key = myid+"I", body="[x] "+ myid+" crashed")
+	crashed = True
+    message = body.split(',')
+    print(message)
+    sender_id = message[0]
+    message_type = message[1]
+    print("[x] message received from " + sender_id + " of type " + message_type)
+    if message_type == "request":
+        get_request(sender_id)
+    elif message_type == "privilege":
+        get_privilege()
+
+
+channel.basic_consume(callback,
+                      queue="I"+myid,
+                      no_ack=True)
+
+channel.basic_consume(callback,
+                      queue=neighbor1+myid,
+                      no_ack=True)
+
+if (len(sys.argv) > 4):
+	channel.basic_consume(callback,
+                      queue=sys.argv[4]+myid,
+                      no_ack=True)
+
+if (len(sys.argv) > 5):
+	channel.basic_consume(callback,
+                      queue=sys.argv[5]+myid,
+                      no_ack=True)
+
+channel.start_consuming()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
